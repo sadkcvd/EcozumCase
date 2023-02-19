591517a536b355332eefe660fe53ec7fe65bc7e1
Sadık Çavdar <93337287+sadkcvd@users.noreply.github.com>
2023-02-19 23:57:57 +0300

    Update packagesReducer.js

--- src/redux/reducers/packagesReducer.js
+++ src/redux/reducers/packagesReducer.js
@@ -6,7 +6,7 @@ export default function packagesReducer(state = initialState, action) {
         case actionTypes.GET_PACKAGES_START:   
             return { ...state, isLoading: true, message: ''};     
         case actionTypes.GET_PACKAGES_SUCCESS:   
-            return { ...state, packages: action.payload, isLoading: false };             //action'dan gelen packages'leri (yani api'den gelen) packages state'ine set edip dönderiyoruz.
+            return { ...state, packages: action.payload, isLoading: false };
         case actionTypes.GET_PACKAGES_ERROR:
             return { ...state, message: action.payload, isLoading: false };
         default:
